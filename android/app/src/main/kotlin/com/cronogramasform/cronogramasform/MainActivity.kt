9b656e423afff770e0b91091c831c17130cb66ad
Rafael <rcnelli@hotmail.com>
2023-07-05 16:57:59 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/cronogramasform/cronogramasform/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cronogramasform.cronogramasform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pagina.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Pagina extends StatefulWidget {
+  const Pagina({super.key});
+
+  @override
+  State<Pagina> createState() => _PaginaState();
+}
+
+class _PaginaState extends State<Pagina> {
+  final List<String> _lista = ["Aula 1", "Aula 2", "Aula 3", "Aula 4"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Lista de Aulas"), backgroundColor: Colors.orange),
+      body: Container(
+        padding: const EdgeInsets.all(200),
+        child: Center(
+            child: ListView(
+          children: _lista.map((text) {
+            return ListTile(
+              tileColor: Colors.orange,
+              title: Text(text),
+              trailing: const Icon(Icons.book),
+            );
+          }).toList(),
+        )),
+      ),
+    );
+  }
+}
